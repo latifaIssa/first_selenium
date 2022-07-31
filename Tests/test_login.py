8c5696cea382ea30f02027614e1928965359ee18
Latifa Masri <lmasri@asaltech.com>
2022-07-31 09:06:46 +0300

    login testing

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -28,15 +28,38 @@ class TestLogin(Base):
         login_page.make_sure_that_username_field_exist()
         login_page.make_sure_that_login_button_exist()
 
-        # valid_username = admin and valid_pass = admin
+        # valid_username = tomsmith and valid_pass = SuperSecretPassword!
+        username = "tomsmith"
+        password = 'SuperSecretPassword!'
         # Fill the form with invalid username and valid password
         login_page.fill_username_login('latifa')
-        login_page.fill_password_login('admin')
+        login_page.fill_password_login(password)
         login_page.click_login_button()
         time.sleep(1)
         # get message error
-        # message_error = login_page.get_validation_error_message()
-        # # make sure that the message error correct
-        # assert message_error == 'Your username is invalid!'
+        message_error = login_page.get_validation_error_message()
+        # make sure that the message error correct
+        assert message_error == 'Your username is invalid!\n×'
+
+        # Fill the form with valid username and invalid password
+        login_page.fill_username_login(username)
+        login_page.fill_password_login('aaaa')
+        login_page.click_login_button()
+        time.sleep(1)
+        # get message error
+        message_error = login_page.get_validation_error_message()
+        # make sure that the message error correct
+        assert message_error == 'Your password is invalid!\n×'
+
+        # Fill the form with valid username and valid password
+        login_page.fill_username_login(username)
+        login_page.fill_password_login(password)
+        login_page.click_login_button()
+        time.sleep(1)
+        # get message
+        message = login_page.get_validation_error_message()
+        # make sure that the message correct
+        assert message == 'You logged into a secure area!\n×'
+
 
 
