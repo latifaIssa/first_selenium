bcac34f59d91353c9a98ccbc512dde58611b703f
Latifa Masri <lmasri@asaltech.com>
2022-08-22 10:01:09 +0300

    file uplaod

--- Locators/file_upload_locators.py
+++ Locators/file_upload_locators.py
@@ -1,2 +1,3 @@
 file_upload_xpath = '//a[@href="/upload" and (text()="File Upload")]'
 choose_file_id = 'file-upload'
+upload_btn_id = 'file-submit'
--- Tests/test_upload_file.py
+++ Tests/test_upload_file.py
@@ -5,7 +5,7 @@ import pytest
 
 from Base.base import Base
 from selenium.webdriver.common.by import By
-from Locators.file_upload_locators import file_upload_xpath, choose_file_id
+from Locators.file_upload_locators import file_upload_xpath, choose_file_id, upload_btn_id
 
 
 @pytest.mark.usefixtures('set_up')
@@ -21,9 +21,28 @@ class TestUploadFile(Base):
         page_header = driver.find_element(By.TAG_NAME, 'h3')
         assert page_header.text == 'File Uploader'
 
-        # download image
+        # Click on upload without choose a file
+        upload_btn = driver.find_element(By.ID, upload_btn_id)
+        upload_btn.click()
+        page_header_error = driver.find_element(By.TAG_NAME, 'h1')
+        time.sleep(2)
+        assert page_header_error.text == 'Internal Server Error'
+
+        # upload file
+        driver.back()
+        time.sleep(1)
         choose_file = driver.find_element(By.ID, choose_file_id)
-        choose_file.click()
-        time.sleep(5)
+        driver.execute_script("arguments[0].style.display = 'block';", choose_file)
+        choose_file.send_keys(r'C:\Users\lmasri\Documents\a.json')
+        time.sleep(2)
+        upload_btn = driver.find_element(By.ID, upload_btn_id)
+        upload_btn.click()
+
+        # check
+        page_header_uploaded = driver.find_element(By.TAG_NAME, 'h3')
+        file_name = driver.find_element(By.CLASS_NAME, 'text-center').text
+        assert page_header_uploaded.text == 'File Uploaded!'
+        assert file_name == 'a.json'
+        time.sleep(1)
 
 
